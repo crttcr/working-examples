633e0b9c3d51570897b8be4306b10a912e0c1b53
Reid <reid@foobox.com>
2016-11-27 10:17:35 -0600

    Revised after discussion with Z

--- git/rebase.workflow.sh
+++ git/rebase.workflow.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-## This script captures a sequence of steps to create a pull request
-## for a feature branch that has all intermediate commits squashed
-## into a single commit on a PR-specific branch.
-##
-## It also performs a rebase based on the main development branch which
-## has presumably advanced while the feature has been under development.
-##
+################################################################################
+##                                                                            ##
+## This script captures a sequence of steps to create a pull request          ##
+## for a feature branch that has all intermediate commits squashed            ##
+## into a single commit on a PR-specific branch.                              ##
+##                                                                            ##
+## It also incorporates a rebase onto the main development branch which       ##
+## has presumably advanced while the feature has been under development.      ##
+##                                                                            ##
+################################################################################
 
 ## Assumptions and Definitions
 ##
-## Your feature branch where development has occured
+## Name of branch where feature development has occured
 ##
-export BRANCH=feature/inspection-extract
+export BRANCH=f/some-feature
 
-## Update your local develop branch to incoroprate all changes from remote
+## Update your local master branch to incoroprate all changes from remote
 ##
-git checkout develop
+git checkout master
 git pull
 
 ## Jump onto feature branch
@@ -37,16 +40,25 @@ git commit --amend -m "Feature implementation ..."
 ## have to redo all of the previous steps if you want to restart.
 ## The -u options sets this branch to have a tracking branch.
 ##
-git push -u origin PR-${BRANCH}
+git push -u origin pr-${BRANCH}
 git fetch
-git rebase origin/develop
+git rebase origin/master
 git status
-git push -f 
 
-## NOW FIX ANY COMPILE ISSUES DUE TO PULLING IN LATEST CHANGES
-git add .
-git commit -m "Rebase PR onto origin/develop & update feature"
-git push
+##
+## FIX ANY ISSUES DUE TO PULLING IN LATEST CHANGES
+##
+
+## If you prefer only a single commit instance in the history
+## rather than 2, you can finish with this "commit"
+##
+## git add .
+## git commit --amend --no-edit
+##
+## otherwise, finish
+##
+git commit -a -m "Rebase PR onto origin/master & update feature"
+git push -f 
 
 
 
