95034f80e591d872a38f90856918c5704798d118
Reid <reid@foobox.com>
2017-03-11 12:15:13 -0600

    doc

--- java/src/main/java/spring/data/jdbc/template/SpitterRepository.java
+++ java/src/main/java/spring/data/jdbc/template/SpitterRepository.java
@@ -11,9 +11,48 @@ import spring.data.domain.Spitter;
  */
 public interface SpitterRepository
 {
+	/**
+	 * Returns the number of records in the Spitter table
+	 *
+	 * @return the number of records in the Spitter database
+	 */
 	long count();
+
+	/**
+	 * Finds the Spitter with the specified id, or null if not found.
+	 *
+	 * @param id the id of the desired Spitter record
+	 * @return the Spitter domain object representing the database record, or null
+	 */
 	public Spitter findOne(Long id);
+
+	/**
+	 * Returns a list of all the Spitters in the database, or an empty list if
+	 * there are none.
+	 *
+	 * @return List of Spitter objects representing the database records in the Spitter table.
+	 */
 	public List<Spitter> find();
+
+	/**
+	 * Performs an Upsert of the Spitter provided as an argument.
+	 * Requires that the Spitter argument not be null or a NullPointerException will be thrown.
+	 *
+	 *
+	 * @param spitter the Spitter domain object to save
+	 * @throws NullPointerException if the spitter argument is null
+	 *
+	 * @return a Spitter object as available after the save operation
+	 *
+	 */
 	public Spitter save(Spitter spitter);
+
+	/**
+	 * Returns the single Spitter with the given username or null if not found.
+	 *
+	 * @param username the name to search for.
+	 * @throws NullPointerException if the username argument is null
+	 * @return the spitter with the given username or null if there isn't one
+	 */
 	public Spitter findByUsername(String username);
 }
