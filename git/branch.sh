fa62b54222425ab82d79c7f073a9a0ea8cce62f5
reid <reid@foobox.com>
2017-04-23 08:30:13 -0500

    Added rename

--- git/branch.sh
+++ git/branch.sh
@@ -7,3 +7,14 @@ git branch -
 ## List upstream branches
 ##
 git branch -vv
+
+## Rename a local branch
+##
+git branch -m oldname newname
+
+## Rename a remote branch means pushing the
+## newly named branch and then deleting the old
+##
+git push -u newname
+git push origin :oldname
+
