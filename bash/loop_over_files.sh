e8a54d6d76774fc40032eba8fc12976a6edb5b4c
reid <reid@foobox.com>
2017-04-30 15:37:44 -0500

    doc

--- bash/loop_over_files.sh
+++ bash/loop_over_files.sh
@@ -1,1 +1,4 @@
-for f in *.c; do echo $f; cp $f $f.backup; done
+## This is a reminder of how to loop over files using
+## the bash shell to perform operations on a set of files
+##
+$ for f in *.c; do echo $f; cp $f $f.backup; done
