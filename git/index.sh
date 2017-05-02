08975704a60aa7d40b49a7933c2626dd706d0578
Reid Turner <crttcr@users.noreply.github.com>
2017-05-02 12:31:07 -0500

    Update index.sh

--- git/index.sh
+++ git/index.sh
@@ -5,3 +5,13 @@
 ##
 git ls-files
 
+## Remove a file from the index only
+##
+git rm --cached [file]
+
+## Remove a file from the index AND the working tree
+##
+## If the staged content doesn't match the tip of the branch or the file on disk,
+## you get a warning which can be ignored with --force
+##
+git rm [file]
