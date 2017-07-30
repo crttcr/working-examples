8709c5c4d81a27389a93c0daeaf032112b1fef2e
Reid <reid@foobox.com>
2017-07-30 11:55:36 -0500

    s/Autovalue/Lombok/g

--- java/src/main/java/lucene/BookInfo.java
+++ java/src/main/java/lucene/BookInfo.java
@@ -4,43 +4,21 @@ import java.util.Collections;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.google.auto.value.AutoValue;
 
-@AutoValue
-@JsonDeserialize(builder=AutoValue_BookInfo.Builder.class)
+import lombok.Data;
+import lucene.BookInfo.BookInfoBuilder;
+
+@Data
+@lombok.Builder
+@JsonDeserialize(builder=BookInfoBuilder.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public abstract class BookInfo
+public class BookInfo
 {
 	public static final List<BookInfo> EMPTY_LIST =  Collections.<BookInfo>emptyList();
 
-	@JsonProperty("title")
-	public abstract String title();
-	
-	@JsonProperty("authors")
-	public abstract String authors();
-	
-	@JsonProperty("isnb")
-	public abstract String isbn();
+	private String title;
+	private String authors;
+	private String isbn;
 
-	@AutoValue.Builder
-	public abstract static class Builder
-	{
-		@JsonProperty("title")
-		public abstract Builder title(String title);
-
-		@JsonProperty("isbn")
-		public abstract Builder isbn(String isbn);
-
-		@JsonProperty("authors")
-		public abstract Builder authors(String authors);
-
-		public abstract BookInfo   build();
-	}
-	
-	public static Builder builder()
-	{
-		return new AutoValue_BookInfo.Builder();
-	}
 }
