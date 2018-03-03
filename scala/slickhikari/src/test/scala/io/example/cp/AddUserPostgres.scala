4b3b39ba38fb1c26085d00d2b843aa7e0b141993
Reid <reid@foobox.com>
2018-03-02 19:55:47 -0600

    First version of non-pooled PG working

---
+++ scala/slickhikari/src/test/scala/io/example/cp/AddUserPostgres.scala
@@ -0,0 +1,25 @@
+package io.example.cp
+
+import DTO._
+
+import slick.jdbc.PostgresProfile
+
+object AddUserPostgres extends App
+{
+  val db_layer = new DatabaseLayer(PostgresProfile)
+  
+  val u = User(-2, "person@aol.com", "Pers", "On")
+  
+  println(s"Adding user [$u]")
+//  val create = db_layer.User.create
+  val insert = db_layer.User.insert(u)
+  val select = db_layer.User.all
+  
+//  val r1 = SlickHelper.pg_exec(create)
+//  val r2 = SlickHelper.pg_exec(insert)
+  val r3 = SlickHelper.pg_exec(select)
+    
+//  println(s"Create: [$r1]")
+//  println(s"Insert: [$r2]")
+  println(s"Select: [$r3]")
+}
